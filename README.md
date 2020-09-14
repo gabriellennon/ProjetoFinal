7979c377909a798fc97600781cdd4dcccaf90af0
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2020-09-14 19:10:05 -0300

    Changes in system functions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ProjetoFinal
+Esse repositório tem a finalidade de apresentar uma solução sobre o sistema de transito brasileiro como projeto final da matéria de Lógica de Programação I
---
+++ projetosra.py
@@ -0,0 +1,7 @@
+
+lista_projetos = ['Projeto 001 RA São Sebastião', 'Projeto 002 RA Taguatinga']
+
+
+print('Bem-vindo, o que deseja fazer hoje?')
+opcaotarefa = int(input('(1) Pesquisar projeto (2) Cadastrar projeto'))
+
